5fbfaf3e4e91b370ed8ec493905341bf95891606
monster-w <littlemonster_w@qq.com>
2022-08-22 22:36:41 +0800

    创建项目

---
+++ src/main/java/com/monster/base/MonsterBaseApplication.java
@@ -0,0 +1,13 @@
+package com.monster.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonsterBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monster/base/MonsterBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monster.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
