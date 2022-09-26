2639aa5e658127240aadc9831f32b5fa3c5e6ea5
wuhan <littlemonster_w@qq.com>
2022-09-26 11:02:17 +0800

    添加Swagger配置

---
+++ src/main/java/com/monster/base/develop/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.monster.base.develop.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Swagger2 API文档的配置
+ *
+ * @author wuhan
+ * @date 2022/9/26 10:44
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Monster项目API文档")
+                .description("monster-project")
+                .contact(new Contact("monster", "", ""))
+                .version("1.0.0")
+                .build();
+    }
+}
